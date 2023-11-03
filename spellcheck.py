2479166f3d4918d30e980ad28dee41202a8e5e4b
ChauBui00 <c.bui3@share.epsb.ca>
2023-11-03 13:07:52 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -11,9 +11,16 @@ def main():
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
 
-    # Print first 50 values of each list to verify contents
-    print(dictionary[0:50])
-    print(aliceWords[0:50])
+    while True:
+        # Get Menu Selection & Do Stuff
+        selection = getMenuSelection()
+
+        if selection == "1":
+            linearSearchWord(dictionary)
+        elif selection == "5":
+            break
+
+
 # end main()
 
 
@@ -28,20 +35,49 @@ def loadWordsFromFile(fileName):
 # end loadWordsFromFile()
 
 
+def getMenuSelection():
+    print("\nMain Menu")
+    print("1: Spell Check a Wrod(Linear Search)")
+    print("2: Spell Chack a Word(Binary Search)")
+    print("3: Spell Check Alice In Wonderland(Linear Search)")
+    print("5: Exit")
+    return input("\nEnter memu selection (1-5):")
+
+
+def linearSearchWord(dictionary):
+    # Search the dictionary for a user provided word
+    input_Word = input("Enter a single word: ")
+    index = linearSearch(dictionary, input_Word.lower())
+    if index == -1:
+        print(f"{input_Word} not found.")
+    else:
+        print(f"{input_Word} found at position: {index}")
+
+
+def linearSearch(anArray, item):
+    for i in range(len(anArray)):
+        if item == anArray[i]:
+            return i
+    return -1
+
+def binarySearch(anArray, item):
+    # LI = Lower Index
+    LI = 0
+    UI = len(anArray) - 1
+
+    while LI <= UI:
+        # MI = Middle Index
+        MI = (LI + UI)//2
+        if item == anArray[MI]:
+            return MI
+        else:
+            if anArray[MI] < item:
+                LI = MI +1
+            else:
+                UI = MI -1
+    return -1
+
+
+
 # Call main() to begin program
 main()
-
-print("Main Menu")
-print("\n1: Spell Check a Wrod(Linear Search)")
-print("2: Spell Chack a Word(Binary Search)")
-print("3: Spell Check Alice In Wonderland(Linear Search)")
-print("5: Exit")
-input = input("\nEnter memu selection (1-5):")
-
-for word in 
-    if input == "1":
-    elif input == "2":
-    elif input == "3":
-    elif input == "4":
-    elif input == "5":
-        break
